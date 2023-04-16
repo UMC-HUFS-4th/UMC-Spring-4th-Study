8741e68ab647d35a162ac91d233291196054b97b
DAHLIACHOI <cyj000424@naver.com>
2023-04-17 01:29:33 +0900

    1주차 API 실습

---
+++ 달리/umcapi/src/test/java/com/example/umcapi/UmcapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
