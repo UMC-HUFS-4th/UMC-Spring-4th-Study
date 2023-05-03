0ac9d8c7e724d2c3483a8bee46fa168215ddab87
choidongkuen <danaver12@daum.net>
2023-05-03 15:36:17 +0900

    Feat : [UMC Lecture] 게시판 프로젝트 완성

--- 토비/Toby/src/main/java/org/example/repository/BoardRepository.java
+++ 토비/Toby/src/main/java/org/example/repository/BoardRepository.java
@@ -1,6 +1,6 @@
 package org.example.repository;
 
-import org.example.Board;
+import org.example.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
